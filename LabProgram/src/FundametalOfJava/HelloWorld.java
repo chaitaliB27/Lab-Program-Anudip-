95b37da327326171f4e90095fbd2486daf30d1f8
user <user@DESKTOP-AREB2AL>
2023-10-17 22:18:29 +0530

    Added class HelloWorld.java

---
+++ LabProgram/src/FundametalOfJava/HelloWorld.java
@@ -0,0 +1,13 @@
+package FundametalOfJava;
+/*
+ * shortcut:syso+ctrl+space
+ * 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello Everyone!!");
+        System.out.println("WELCOME IN Anudip Foundation");
+	}
+
+}
