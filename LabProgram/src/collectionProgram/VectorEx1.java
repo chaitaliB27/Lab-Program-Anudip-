1589c827b8a9dba5a6c79230d8490bd084d02775
chaitaliB27 <145447766+chaitaliB27@users.noreply.github.com>
2023-11-21 11:05:24 +0530

    Update VectorEx1.java

--- LabProgram/src/collectionProgram/VectorEx1.java
+++ LabProgram/src/collectionProgram/VectorEx1.java
@@ -3,7 +3,24 @@ package collectionProgram;
 public class VectorEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Vector<String> v = new Vector<String>();
+		v.add("Saturday");
+		v.add("Friday");
+		v.add("Monday");
+		v.add(null);
+		v.add("Sunday");
+		v.add("Tuesday");
+		v.add("Thursday");
+		v.add("Wednesday");
+		
+		System.out.println(v);
+		
+		System.out.println("Using Enumeration...");
+		Enumeration<String> e = v.elements();
+		
+		while(e.hasMoreElements()) {
+			System.out.println(e.nextElement());
+		}
 
 	}
 
